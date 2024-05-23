a6dc57d05eff5d95576f574ec378be5993b0c60f
Demigorgan123 <rbanswal123@gmail.com>
2024-05-24 01:15:09 +0530

    adding setTimeout of 20s in customers page to check if the dashboard skeleton is also loading on customer pages or not.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,6 @@
+import { unstable_noStore as noStore} from "next/cache";
 export default async function Page(){
+    noStore();
     await new Promise((resolve) => setTimeout(resolve, 20000));
     return <p>Customer Page</p>
 }
