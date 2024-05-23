b5d4ac05c72c2c849b3e05a519ee878778ecf73c
Demigorgan123 <rbanswal123@gmail.com>
2024-05-24 01:18:17 +0530

    Reverting all the changes from customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,3 @@
-import { unstable_noStore as noStore} from "next/cache";
-export default async function Page(){
-    noStore();
-    await new Promise((resolve) => setTimeout(resolve, 20000));
+export default function Page(){
     return <p>Customer Page</p>
 }
