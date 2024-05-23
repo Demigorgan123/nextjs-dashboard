a772b760dcc780a9f6f44b11258100cecc349f2a
Demigorgan123 <rbanswal123@gmail.com>
2024-05-24 01:25:32 +0530

    adding /overview page in dashboard so that skeleton will only load on dashboard page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
-export default function Page(){
+import { unstable_noStore as noStore } from "next/cache"
+export default async function Page(){
+    noStore();
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     return <p>Customer Page</p>
 }
