9a2312bdae7021282cf6552c7e86b29147f3c73b
Demigorgan123 <rbanswal123@gmail.com>
2024-05-31 02:15:46 +0530

    adding form validation in edit form file

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,7 +10,6 @@ import {
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/action';
 import { useFormState } from 'react-dom';
-import { error } from 'console';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
