bb5cbf68034a40a30013226059029e50468a2084
Demigorgan123 <rbanswal123@gmail.com>
2024-05-30 17:21:05 +0530

    reverting all the changes

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -9,6 +9,10 @@ export default async function Page({params}: {params: {id:string}}) {
         fetchInvoiceById(id),
         fetchCustomers(),
     ])
+
+    if(!invoice){
+      notFound();
+    }
   return (
     <main>
       <Breadcrumbs
