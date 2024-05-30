f6d1e6a527deb2b28acab9fd2416bc9dfda33b41
Demigorgan123 <rbanswal123@gmail.com>
2024-05-30 16:46:54 +0530

    adding error in try block before sql query

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -59,8 +59,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   try {
+    throw new Error("Failed to Delete Invoice");
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
     return{
